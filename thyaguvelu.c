e8eab064eaae53397a06b7e175d18e26fa961543
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-01-24 15:53:03 +0530

    Update thyaguvelu.c

--- thyaguvelu.c
+++ thyaguvelu.c
@@ -2,15 +2,11 @@
 int main()
 {
     int number;
-
-    printf("Enter an integer: ");
+    printf("Enter an number: ");
     scanf("%d", &number);
-
-    // True if the number is perfectly divisible by 2
-    if(number % 2 == 0)
+   if(number % 2 == 0)
         printf("%d is even.", number);
     else
         printf("%d is odd.", number);
-
     return 0;
 }
