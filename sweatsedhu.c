5ffa8805ca20f6105569a1095119a66f2ccfcccf
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-01-24 16:57:20 +0530

    Create sweatsedhu.c

---
+++ sweatsedhu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num, i, sum = 0;    
+    printf("Enter a number is : ");
+    scanf("%d",&n);
+    for(i=1; i <= num; ++i)
+    {
+        sum += i;  
+    }
+    printf("Sum = %d",sum);
+    return 0;
+}
