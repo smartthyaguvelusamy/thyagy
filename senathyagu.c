3068a9a676e94750f2fcd3f3d798b3e35528d8ba
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-01-31 16:29:40 +0530

    Create senathyagu.c

---
+++ senathyagu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int n,m =20,n=40;
+    clrscr();
+    printf("Print Odd Numbers in a given range m to n:\n");
+    for (n = m; n <= n; n++)
+        {
+        if (n % 2 == 1)
+                  printf ("%d ", n);
+         }
+                getch();
+}
