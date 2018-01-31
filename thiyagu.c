dfada6bd385fe5cfeadcc1a7a501d03e790c7e11
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-01-31 16:06:32 +0530

    Create thiyagu.c

---
+++ thiyagu.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,k;
+array[10];
+printf("\n enter the number\n");
+for(i=1;i<=3;i++)
+{
+sum+=i;
+}
+printf("\n sum of the number is"\n);
+{
+sum=sum+i;
+}
