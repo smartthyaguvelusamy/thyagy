063ff377a9fdac9ea52912aedbd544e7c1500ef1
smartthyaguvelusamy <35221750+smartthyaguvelusamy@users.noreply.github.com>
2018-03-14 15:48:05 +0530

    Create thyagyy.c

---
+++ thyagyy.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&n);
+if(num%2==0)
+{
+printf(" %d is power of 2",num);
+}
+else
+{
+printf("not a power of two");
+}
+}
